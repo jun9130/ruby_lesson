6aff8a6353bccb85da322e10ff58bdd5d1f3f2ad
jun9130 <jun.nakama1030@gmail.com>
2020-04-08 13:58:39 +0900

    クラス変数

--- hello.rb
+++ hello.rb
@@ -1,7 +1,25 @@
-def sayHi(name) 
-  puts "こんにちは #{name}くん"
-  score = 80 #メソッド内に定義された変数は外からアクセスできない
+class User
+
+    @@count = 0
+
+
+  def initialize(name)
+    @@count += 1
+    @name = name
+  end
+
+  def sayHi
+    puts "hi! i am #{@name}"
+  end
+
+  def self.info
+    puts "User Class, #{@@count} instances"
+  end
+
 end
 
-sayHi("佐藤")
-put score
+tom = User.new('tom')
+bob = User.new('bob')
+steve = User.new('steve')
+
+User.info
