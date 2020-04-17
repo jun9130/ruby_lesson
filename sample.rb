2a569b03df4540caea39be3d20b9ad87e25132b6
jun9130 <jun.nakama1030@gmail.com>
2020-04-17 22:55:06 +0900

    with_index

--- sample.rb
+++ sample.rb
@@ -1,9 +1,15 @@
-a = ["a", "b", "c", "d", "e"]
-puts a[0,3]
+#ブロック引数iは要素の添え字
+fruits = ['apple', 'orange', 'melon']
+fruits.each_with_index { |fruit, i| puts "#{i}: #{fruit}"}
 
-a = ["a", "b", "c", "d", "e"]
-p a.values_at(0, 3, 4)
+fruits = ['apple', 'orange', 'melon']
+fruits.map.with_index { |fruit, i| puts "#{i}: #{fruit}"}
 
-a = ["a", "b", "c", "d", "e"]
-puts a.last(2)
+#each.with_index(数値)で添え字をその数値から始める
+fruits = ['apple', 'orange', 'melon']
+fruits.each.with_index(1) { |fruit, i| puts "#{i}: #{fruit}"}
+
+fruits = ['apple', 'orange', 'melon']
+fruits.map.with_index(100) { |fruit, i| puts "#{i}: #{fruit}"}
+
 
