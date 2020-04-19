056d937bb6ba673ce706f58bdd861a91a289e48c
jun9130 <jun.nakama1030@gmail.com>
2020-04-19 18:23:27 +0900

    ハッシュ

--- sample.rb
+++ sample.rb
@@ -1,38 +1,14 @@
-# while 条件式
-#   繰り返したい処理
-# end
-a = []
-while a.size < 5
-  a << 1
-end
-puts a
+要素の追加
+ハッシュ[キー] = 値
+currencies = {japan: "yen", us: "dollar"}
+currencies[:italy] = 'euro'
+puts currencies
 
-# for 変数 in 配列やハッシュ
-#   繰り返し処理
-# end
-numbers = [1, 2, 3, 4]
-  sum = 0
-  for n in numbers
-    sum += n
-  end
-puts sum
-
-# each文での書き換え
-numbers = [1, 2, 3, 4]
-  sum = 0
-  numbers.each do |n|
-    sum += n
-  end
-  puts sum
-
-
-# break 
-numbers = [1, 2, 3, 4, 5].shuffle
-numbers.each do |n|
-  puts n
-  break if n == 5
+currencies = {japan: "yen", us: "dollar"}
+currencies.each do |key, value|
+  puts "#{key}の通貨は「#{value}」です"
 end
 
-
-
-
+currencies = {japan: "yen", us: "dollar"}
+currencies.delete(:japan)
+puts currencies
