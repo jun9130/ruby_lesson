80b19fc3ba1cb114dc3cf05852e618a8452d6853
jun9130 <jun.nakama1030@gmail.com>
2020-04-19 22:06:06 +0900

    ハッシュテスト

--- sample.rb
+++ sample.rb
@@ -1,14 +1,12 @@
-要素の追加
-ハッシュ[キー] = 値
-currencies = {japan: "yen", us: "dollar"}
-currencies[:italy] = 'euro'
-puts currencies
+currencies = {japan: "yen", us: "dollar", india:"rupee"}
 
-currencies = {japan: "yen", us: "dollar"}
-currencies.each do |key, value|
-  puts "#{key}の通貨は「#{value}」です"
-end
+# puts currencies.keys
+# puts currencies.values
+puts currencies.has_key?(:japan)
+puts currencies.has_key?(:china)
 
-currencies = {japan: "yen", us: "dollar"}
-currencies.delete(:japan)
-puts currencies
+#エイリアスメソッド include?
+puts currencies.include?(:japan)
+puts currencies.include?(:china)
+
+
