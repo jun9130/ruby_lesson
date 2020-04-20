eae8df0107b2d95b94c7ef451b0aef334dcadda0
jun9130 <jun.nakama1030@gmail.com>
2020-04-20 22:29:05 +0900

    クラスの定義、インスタンス変数、クラスメソッド

--- sample.rb
+++ sample.rb
@@ -1,7 +1,76 @@
-currencies = {japan: "yen", us: "dollar", india:"rupee"}
+#クラスの定義
+# class クラス
+# end
 
-p currencies.to_a
-p currencies
+class User
+  def initialize(name, age)
+    puts "#{name}は今年#{age}歳です!"
+  end
+end
 
-h = Hash.new('hello')
-puts h[:aaaaa]
+User.new('taro', 20)
+User.new('jiro', 32)
+
+
+#インスタントメソッドの定義
+class User
+  def hello
+    puts "インスタントメソッドが呼ばれる。"
+  end
+end
+
+user = User.new
+user.hello
+
+#インスタンス変数
+class User
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name #インスタンス変数
+    @age = age
+  end
+
+  def hello
+    "Hello, I am #{@name}."
+  end
+
+end
+
+user = User.new('taro', 22)
+puts user.hello
+puts user.name
+puts user.age
+
+
+#クラスメソッドの定義
+# class クラス名
+#   def self.クラスメソッド
+#     クラスメソッドの処理
+#   end
+# end
+
+
+class User
+  def initialize(name)
+  @name = name
+  end
+
+  def self.create_users(names)
+    names.map do |name|
+      User.new(name)
+    end
+  end
+
+  def hello
+    "Hello, I am #{@name}"
+  end
+
+end
+
+names = ['taro', 'yuta', 'mei']
+users = User.create_users(names)
+users.each do |user|
+  puts user.hello
+end
+
