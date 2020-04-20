dfd7533131a6c98d021782f6315c35ee3e4e9049
jun9130 <jun.nakama1030@gmail.com>
2020-04-20 15:36:06 +0900

    ハッシュ変換

--- sample.rb
+++ sample.rb
@@ -1,12 +1,7 @@
 currencies = {japan: "yen", us: "dollar", india:"rupee"}
 
-# puts currencies.keys
-# puts currencies.values
-puts currencies.has_key?(:japan)
-puts currencies.has_key?(:china)
+p currencies.to_a
+p currencies
 
-#エイリアスメソッド include?
-puts currencies.include?(:japan)
-puts currencies.include?(:china)
-
-
+h = Hash.new('hello')
+puts h[:aaaaa]
