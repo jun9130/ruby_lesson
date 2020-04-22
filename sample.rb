2386ec075cd12881e0097a1eeeb21506fe4bec57
jun9130 <jun.nakama1030@gmail.com>
2020-04-22 21:36:30 +0900

    elseifを使わないfizzbuzz

--- sample.rb
+++ sample.rb
@@ -1,76 +1,16 @@
-#クラスの定義
-# class クラス
-# end
-
-class User
-  def initialize(name, age)
-    puts "#{name}は今年#{age}歳です!"
+num = 1
+while num < 101
+  str = ""
+  if num % 3 == 0
+    str = str + "fizz"
   end
+
+  if num % 5 == 0
+    str = str + "buzz"
+  end
+  if str == ""
+    str = str + num.to_s
+  end
+  puts str
+  num += 1
 end
-
-User.new('taro', 20)
-User.new('jiro', 32)
-
-
-#インスタントメソッドの定義
-class User
-  def hello
-    puts "インスタントメソッドが呼ばれる。"
-  end
-end
-
-user = User.new
-user.hello
-
-#インスタンス変数
-class User
-  attr_accessor :name, :age
-
-  def initialize(name, age)
-    @name = name #インスタンス変数
-    @age = age
-  end
-
-  def hello
-    "Hello, I am #{@name}."
-  end
-
-end
-
-user = User.new('taro', 22)
-puts user.hello
-puts user.name
-puts user.age
-
-
-#クラスメソッドの定義
-# class クラス名
-#   def self.クラスメソッド
-#     クラスメソッドの処理
-#   end
-# end
-
-
-class User
-  def initialize(name)
-  @name = name
-  end
-
-  def self.create_users(names)
-    names.map do |name|
-      User.new(name)
-    end
-  end
-
-  def hello
-    "Hello, I am #{@name}"
-  end
-
-end
-
-names = ['taro', 'yuta', 'mei']
-users = User.create_users(names)
-users.each do |user|
-  puts user.hello
-end
-
