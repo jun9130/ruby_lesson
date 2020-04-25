37be4a36b3c55cef801c439b9b30d9bc7fdb4d3d
jun9130 <jun.nakama1030@gmail.com>
2020-04-25 13:55:21 +0900

    FizzBuzz

--- sample.rb
+++ sample.rb
@@ -1,45 +1,18 @@
-#問1
-# def puts_something(something)
-#   puts "#{something}!"
-# end
-# puts "何か値を入力してください・・・"
-# string = gets.chomp
-# puts_something(string)
+def fizz_buzz
+  num = 1
+  while num < 101 do
+    if num % 15 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+    num += 1  
+  end
+  
+end
 
-
-#問2
-# def multiplication(num1, num2)
-#   puts "#{num1}と#{num2}をかけた答えは#{num1 * num2}です！"
-# end
-
-# puts "最初の数字を入力してください"
-# num1 = gets.to_i
-
-# puts "２番目の数字を入力してください"
-# num2 = gets.to_i
-
-# multiplication(num1, num2)
-
-#問3
-# def fruits_box(fruits, num)
-#   puts "#{num}番目の要素は#{fruits[num - 1]}です！"
-# end
-
-# fruits_box = ["apple", "orange", "cherry"]
-
-# puts "何がでるかな？取り出したい要素の順番を入力してください"
-
-# num = gets.to_i
-
-# fruits_box(fruits_box, num)
-
-#問4
-
-movie = {"title" => "ハリーポッター", "genre" => "ファンタジー", "year" => "2001年"}
-
-puts "以下から一つを選んで入力してください。
-・title
-・genre
-・year"
-item = gets.chomp
-puts movie[item]
+fizz_buzz
