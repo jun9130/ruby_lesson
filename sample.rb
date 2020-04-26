5eca9b84b032968c8356c52e641e6da594e94f69
jun9130 <jun.nakama1030@gmail.com>
2020-04-26 22:04:40 +0900

    アルゴリズム

--- sample.rb
+++ sample.rb
@@ -1,18 +1,22 @@
-def fizz_buzz
-  num = 1
-  while num < 101 do
-    if num % 15 == 0
-      puts "FizzBuzz"
-    elsif num % 3 == 0
-      puts "Fizz"
-    elsif num % 5 == 0
-      puts "Buzz"
-    else
-      puts num
-    end
-    num += 1  
-  end
-  
+def addition(a, b)
+a + b
 end
 
-fizz_buzz
+def multiplication(a,b)
+  a * b
+end
+
+def slice_num(num)
+  # 10の位
+  tens_place = (num / 10) % 10
+  # 1の位
+  ones_place = (num / 1) % 10
+  return tens_place, ones_place
+end
+
+puts "二桁の整数を入力してください"
+input = gets.to_i
+X, Y = slice_num(input)
+add_result = addition(X, Y)
+multiple_result = multiplication(X, Y)
+puts "足し算結果と掛け算結果の合計値は#{add_result + multiple_result}です"
