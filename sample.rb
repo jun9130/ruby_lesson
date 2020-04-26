2e1510827671e94d21697e37d4d9548379606b10
jun9130 <jun.nakama1030@gmail.com>
2020-04-27 07:22:44 +0900

    問題: Dateオブジェクト

--- sample.rb
+++ sample.rb
@@ -1,10 +1,10 @@
-num = 0
-while num < 100 do
-  puts num
-  num += 1
-end
+require "date"
 
-num = 0
-for num in 0..99 do
-  puts num
+day = Date.today.wday
+days = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"]
+
+if day == 5
+  puts "今日は#{days[day]}だ！！！"
+else
+  puts "今日は#{days[day]}"
 end
