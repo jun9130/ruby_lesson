515d7d23fe9acc4ed1f953c0bad98644d8bac878
jun9130 <jun.nakama1030@gmail.com>
2020-04-28 08:56:08 +0900

    問題: クラスとインスタンスを利用したプログラムの作成

--- sample.rb
+++ sample.rb
@@ -1,28 +1,11 @@
-class Article
+class Book
+  attr_reader :title, :price
 
-  def initialize(author, title, content)
-    @author = author
+  def initialize(title, price)
     @title = title
-    @content = content
+    @price = price
   end
-
-  def author
-    "著者: #{@author}"
-  end
-
-  def title
-    "タイトル: #{@title}"
-  end
-
-  def content
-    "本文: #{@content}"
-  end
-
-
 end
 
-article = Article.new("阿部", "Rubyの素晴らしさについて", "Awesome Ruby!")
-
-puts "著者: #{article.author}"
-puts "タイトル: #{article.title}"
-puts "本文: #{article.content}"
+book = Book.new("ハリーポッター", 1000)
+puts book.title
