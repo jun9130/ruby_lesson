2d633f8f4515ab75016dd545977d9b460ef609ac
jun9130 <jun.nakama1030@gmail.com>
2020-04-28 09:00:07 +0900

    問題: クラスとインスタンスを利用したプログラム

--- sample.rb
+++ sample.rb
@@ -1,11 +1,14 @@
-class Book
-  attr_reader :title, :price
+def search(target_num, input)
 
-  def initialize(title, price)
-    @title = title
-    @price = price
+  input.each_with_index do |num, index|
+    if num == target_num
+      puts "#{index + 1}番目にあります"
+      return
+    end
   end
+  puts "その数は含まれていません"
 end
 
-book = Book.new("ハリーポッター", 1000)
-puts book.title
+input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
+
+search(3, input)
