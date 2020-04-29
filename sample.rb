2de608f7172b4967a59cde077691af8f2919d59e
jun9130 <jun.nakama1030@gmail.com>
2020-04-29 23:49:14 +0900

    問題: うるう年

--- sample.rb
+++ sample.rb
@@ -1,18 +1,26 @@
-def fizzbuzz(max_num)
-
-  (1..max_num).each do |num|
-    if num % 15 == 0
-      puts "FizzBuzz"
-    elsif num % 5 == 0
-      puts "Buzz"
-    elsif num % 3 == 0
-      puts "Fizz"
+def get_days(year, month)
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if month == 2
+    if year % 4 == 0
+      if year % 100 == 0 && year % 400 != 0
+        days = 28
+      else
+        days = 29
+      end
     else
-      puts num
+      days = 28
     end
+  else
+    days = month_days[month - 1]
   end
+
+  return days
 end
 
-puts 'いくつまで数えますか？'
-num = gets.to_i
-fizzbuzz(num)
+puts "年を入力してください："
+year = gets.to_i
+puts "月を入力してください："
+month = gets.to_i
+
+days = get_days(year, month)
+puts "#{year}年#{month}月は#{days}日間あります"
