d9214b43da5fcd3fee3d49108ec86288307b42c7
jun9130 <jun.nakama1030@gmail.com>
2020-04-29 17:32:26 +0900

    色々な引数

--- sample.rb
+++ sample.rb
@@ -1,27 +1,22 @@
-user_data = [
-  {
-    user: {
-        profile: {
-            name: 'George'
-        }
-    }
-  },
-  {
-    user: {
-        profile: {
-          name: 'Alice'
-        }
-    }
-  },
-  {
-    user: {
-        profile: {
-            name: 'Taro'
-        }
-    }
-  }
-]
-
-user_data.each do |u|
-  puts u[:user][:profile][:name]
+#デフォルト値付き引数
+def method1(num=3)
+  puts num
 end
+
+method1
+method1(10)
+
+#可変長引数
+def method2(*num)
+  p num
+end
+
+method2(10, 20)
+method2(1000, "aaa", 50)
+
+#キーワード引数
+def method3(name:)
+  puts name
+end
+
+method3(name: "yamada")
