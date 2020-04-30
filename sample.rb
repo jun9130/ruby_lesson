017e11edb1422f69543aa7f411807e9d12495486
jun9130 <jun.nakama1030@gmail.com>
2020-04-30 17:33:03 +0900

    問題: 条件分岐ドリル58

--- sample.rb
+++ sample.rb
@@ -1,26 +1,13 @@
-def get_days(year, month)
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  if month == 2
-    if year % 4 == 0
-      if year % 100 == 0 && year % 400 != 0
-        days = 28
-      else
-        days = 29
-      end
-    else
-      days = 28
-    end
+def sleep_in(is_weekday, is_vacation)
+  if (is_weekday != true) || (is_vacation == false)
+    puts "True"
   else
-    days = month_days[month - 1]
+    puts "False"
   end
-
-  return days
 end
 
-puts "年を入力してください："
-year = gets.to_i
-puts "月を入力してください："
-month = gets.to_i
 
-days = get_days(year, month)
-puts "#{year}年#{month}月は#{days}日間あります"
+is_weekday = true
+is_vacation = false
+
+sleep_in(is_weekday,is_vacation)
