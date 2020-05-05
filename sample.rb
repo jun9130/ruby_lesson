2853a9601ade6cc490624f0e1277d69990754fdc
jun9130 <jun.nakama1030@gmail.com>
2020-05-05 22:16:04 +0900

    問題: ドリル63

--- sample.rb
+++ sample.rb
@@ -1,5 +1,13 @@
-module ProductsHelper
-  def converting_to_jpy(price)
-    "#{price.to_s(:delimited, delimiter: ',')}円"
+def array123(nums)
+  if (nums.include?(1) && nums.include?(2) && nums.include?(3))
+    puts "True"
+  else
+    puts "False"
   end
 end
+
+nums = [1, 2, 3]
+array123(nums)
+
+nums = [1, 2, 4]
+array123(nums)
