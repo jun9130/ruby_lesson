4e7c6eb889c5076a3bd7698a59b207e9790fba96
jun9130 <jun.nakama1030@gmail.com>
2020-05-08 22:59:28 +0900

    問題: API

--- sample.rb
+++ sample.rb
@@ -1,13 +1,3 @@
-def array123(nums)
-  if (nums.include?(1) && nums.include?(2) && nums.include?(3))
-    puts "True"
-  else
-    puts "False"
-  end
+def left2(str)
+  puts str[2..-1] + str[0..1]  
 end
-
-nums = [1, 2, 3]
-array123(nums)
-
-nums = [1, 2, 4]
-array123(nums)
