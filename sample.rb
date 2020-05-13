f48fee852f80d029d897830bb8955d37dc5d2b22
jun9130 <jun.nakama1030@gmail.com>
2020-05-13 22:00:11 +0900

    問題: 条件分岐

--- sample.rb
+++ sample.rb
@@ -1,5 +1,5 @@
-def cat_dog(str)
-  if str.include?("cat") && str.include?("dog")
+def in1to10(num,outside_mode)
+  if (num >= 1 && num <= 10) || outside_mode
     puts "True"
   else
     puts "False"
