4e131afc80c609658f34bdc4095ff45f39c8cdb0
jun9130 <jun.nakama1030@gmail.com>
2020-05-31 22:31:59 +0900

    ドリル

--- sample.rb
+++ sample.rb
@@ -1,5 +1,6 @@
-def cat_dog(str)
-  if str.include?("cat") && str.include?("dog")
+def near_ten(num)
+  quotient = num % 10
+  if quotient  <= 2 || quotient >= 8
     puts "True"
   else
     puts "False"
