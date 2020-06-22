edb30cc3718cd72b6d3d6ff5497a42299f5fc449
jun9130 <jun.nakama1030@gmail.com>
2020-06-22 23:18:05 +0900

    fizzbuzz

--- sample.rb
+++ sample.rb
@@ -1,3 +1,18 @@
-def left2(str)
-  puts str[2..-1] + str[0..1]  
+def fizzbuzz(max_num)
+
+  (1..max_num).each do |num|
+    if num % 15 == 0
+      puts "FizzBuzz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    else
+      puts num
+    end
+  end
 end
+
+puts 'いくつまで数えますか？'
+num = gets.to_i
+fizzbuzz(num)
