1c1bc846aeb4b2eb910cdc3d2287970b6ead485b
jun9130 <jun.nakama1030@gmail.com>
2020-07-07 13:36:32 +0900

    問題

--- sample.rb
+++ sample.rb
@@ -1,7 +1,6 @@
-def xyz_there(str) 
-  if str.include?(".xyz")
-    puts "False"
-  elsif str.include?("xyz")
+def near_ten(num)
+  quotient = num % 10
+  if quotient  <= 2 || quotient >= 8
     puts "True"
   else
     puts "False"
