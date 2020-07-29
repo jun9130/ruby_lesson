2a360c99d91248675b366ebf0a04e93420df51db
jun9130 <jun.nakama1030@gmail.com>
2020-07-29 14:23:42 +0900

    繰り返し

--- sample.rb
+++ sample.rb
@@ -1,8 +1,10 @@
-def near_ten(num)
-  quotient = num % 10
-  if quotient  <= 2 || quotient >= 8
-    puts "True"
-  else
-    puts "False"
-  end
+input_line = []
+
+for n in 0..4  do
+  puts "#{n + 1}回目の数字入力してください"
+  number = gets.to_i
+  input_line << number
 end
+
+puts "入力した5個の数字の中で最大の数字は「#{input_line.max}」で最小の数字は「#{input_line.min}」です"
+
