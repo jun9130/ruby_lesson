d44f4a39ed4d849b75154efa984f70933004879d
jun9130 <jun.nakama1030@gmail.com>
2020-07-30 22:56:55 +0900

    問題:条件式

--- sample.rb
+++ sample.rb
@@ -1,16 +1,16 @@
 puts "最初の言葉を入れてください"
-wordA = gets.to_s
+firstWord = gets.to_s
 puts "次の言葉を入れてください"
-wordB = gets.to_s
+secondWord = gets.to_s
 
-if wordA == wordB #語句が一致している場合
+if firstWord == secondWord #語句が一致している場合
   puts "OK"
 else
   puts "NG"
 end
 
 
-# if wordA != wordB 語句が一致していない場合
+# if firstWord != secondWord 語句が一致していない場合
 #   puts "NG"
 # else
 #   puts "OK"
