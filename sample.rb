ea0e902444b787648fac1584330d6c6e6f8ead07
jun9130 <jun.nakama1030@gmail.com>
2020-08-04 15:17:22 +0900

    ドリル

--- sample.rb
+++ sample.rb
@@ -1,16 +1,8 @@
-puts "1から100まで好きな数字を入れてください"
-number = gets.to_i
-n = 1
+array = []
 
-number.times do
-  if n % 15 == 0
-    puts "Fizz Buzz"
-  elsif n % 3 == 0
-    puts "Fizz"
-  elsif n % 5 == 0
-    puts "Buzz"
-  else
-    puts n
-  end
-  n = n + 1
-end
+2.times {
+  number = gets.to_i
+  array << number
+}
+
+p array.max - array.min
