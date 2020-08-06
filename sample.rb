be4f9226a2392f33c279b885b2360a1c6a54abb6
jun9130 <jun.nakama1030@gmail.com>
2020-08-06 22:28:20 +0900

    charsメソッド

--- sample.rb
+++ sample.rb
@@ -1,4 +1,3 @@
-puts "数字の半角スペースで区切り2つ入力してください 例: 1 99 "
-number = gets
-num = number.split(" ") #num配列 #splitで要素を分割
-puts "「#{num[0].to_i}」 + 「#{num[1].to_i}」 は 「#{num[0].to_i + num[1].to_i}」 です。" #to_iで数値に変換
+puts 'abcdefghijk'.chars
+p 'abcdefghijk'.chars
+print 'abcdefghijk'.chars
