dc5f8595722e985bc74e91b2308df6c1ea30bd9f
jun9130 <jun.nakama1030@gmail.com>
2020-08-06 13:50:38 +0900

    countメソッド

--- sample.rb
+++ sample.rb
@@ -1,8 +1,14 @@
-array = []
+#countメソッド
+array = [1, 22, 333, 55, 22]
+puts array.count #要素の数
+puts array.count(22) #引数と一致するものの数
 
-2.times {
-  number = gets.to_i
-  array << number
-}
+ary = ['A型', 'O型', 'O型', 'O型', 'AB型', 'O型', 'B型', 'O型']
+puts ary.count
+puts ary.count('O型')
 
-p array.max - array.min
+some_food = []
+puts "食べ物を入力してください"
+food = gets.to_s
+some_food << food
+puts some_food
