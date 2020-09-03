87c46d92dc152ff5f04b91b9bf809932536bc1c0
jun9130 <jun.nakama1030@gmail.com>
2020-09-03 16:29:59 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,6 +1,14 @@
-numbers = []
+array = []
 
-puts "数字を半角スペースで入れてください"
-num = gets
-numbers = num.split(" ")
-puts "最小の数は#{numbers.min}です"
+puts "何個の数字を入れますか？"
+count = gets.to_i
+
+n = 1
+count.times {
+  puts "#{n}回目の数字を入れてください"
+  number = gets.to_i
+  array << number
+  n += 1
+}
+
+puts "最小の数は#{array.min}です。"
