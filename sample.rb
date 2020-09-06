b6cfe1aaa7c9282d1fd11afbd8d71171173b3350
jun9130 <jun.nakama1030@gmail.com>
2020-09-06 20:45:11 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,3 +1,8 @@
-first = gets.chomp
-second = gets.chomp
-puts "#{first}@#{second}"
+puts "所持金を入力してください"
+num = gets.to_i
+
+if num < 10000
+  puts "10000円チャージして残高は#{num + 10000}円になりました。"
+else
+  puts "残高は#{num}円です。"
+end
