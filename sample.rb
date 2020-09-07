a2c6e923326287317b82d86b1b68251a24f09381
jun9130 <jun.nakama1030@gmail.com>
2020-09-07 21:54:08 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -3,8 +3,15 @@ num = gets.to_i
 
 i = 0
 numbers = []
-num.times {
+
+#timesからの書き換え
+while i < num
   i += 1
   numbers << i
-}
+end
+
+# num.times {
+#   i += 1
+#   numbers << i
+# }
 puts "1から#{num}までの和は#{numbers.sum}です。"
