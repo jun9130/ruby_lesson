33bded35006cefe95f2fb30fedcfed32c6957adc
jun9130 <jun.nakama1030@gmail.com>
2020-09-09 13:58:49 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,4 +1,14 @@
-a = gets.to_i
-b = gets.to_i
+input = gets.to_i
 
-puts a % b
+array = []
+n = 1 
+
+input.times {
+  puts "#{n}回目の数字を入れてください"
+  num = gets.to_i
+  array << num 
+  n += 1
+}
+
+puts "配列の中の最大の数は#{array.max}です。" #arrayクラスmax
+
