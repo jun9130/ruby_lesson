71bc02e9c98416f2b1e97793cf8665233a7ef39f
jun9130 <jun.nakama1030@gmail.com>
2020-09-10 22:46:41 +0900

    upcaseメソッド downcaseメソッド

--- sample.rb
+++ sample.rb
@@ -1,2 +1,7 @@
-ary = [1,2,3]
-p ary.join(' ')
+puts "小文字の英単語を入れてください"
+word = gets
+puts word.upcase #upcase→大文字の文字列を返す
+
+puts "小文字の英単語を入れてください"
+word = gets
+puts word.downcase #downcase→小文字の文字列を返す
