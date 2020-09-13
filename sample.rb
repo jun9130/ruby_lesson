35aca5e42f90b70f13a0a1d5bc12c3f72a52dbb1
jun9130 <jun.nakama1030@gmail.com>
2020-09-13 13:59:47 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,13 +1,3 @@
-input_num = gets.to_i
+input_match = gets.to_i
 
-#変数に代入
-num =
-if input_num > 100
-  puts "#{input_num}は100以上です"
-elsif input_num > 50
-  puts "#{input_num}は100以下50以上です"
-else 
-  puts "#{input_num}は50以下です"
-end
-
-num #メソッド呼び出し
+puts input_match * (input_match - 1) / 2
