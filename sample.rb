3544c5ce03917b07c7b2f456553e9ff46e0746c1
jun9130 <jun.nakama1030@gmail.com>
2020-09-17 14:46:51 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,10 +1,4 @@
-input_num = gets.chomp #chompを使う
+n = gets.to_i
+m = gets.to_i
 
-
-if input_num.length == 3
-  puts input_num
-elsif input_num.length == 2
-  puts "0#{input_num}"
-else 
-  puts "00#{input_num}"
-end
+puts n / m
