262bdd25b70bd4d147043ee1cc029387b8301864
jun9130 <jun.nakama1030@gmail.com>
2020-09-21 16:16:41 +0900

    puts print pメソッド

--- sample.rb
+++ sample.rb
@@ -1,2 +1,20 @@
-check = gets
-puts check.count('c')  
+
+puts "hello"
+puts 123
+
+type = ["A型", "B型", "O型", "AB型"]
+puts type
+
+
+print "hello"
+print 123
+
+type = ["A型", "B型", "O型", "AB型"]
+print type
+
+
+p "hello"
+p 123
+
+type = ["A型", "B型", "O型", "AB型"]
+p type
