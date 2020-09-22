dbf4f1fce44a7025e726819c506deac0d9a80955
jun9130 <jun.nakama1030@gmail.com>
2020-09-22 15:19:17 +0900

    破壊的メソッド

--- sample.rb
+++ sample.rb
@@ -1,20 +1,4 @@
-
-puts "hello"
-puts 123
-
-type = ["A型", "B型", "O型", "AB型"]
-puts type
-
-
-print "hello"
-print 123
-
-type = ["A型", "B型", "O型", "AB型"]
-print type
-
-
-p "hello"
-p 123
-
-type = ["A型", "B型", "O型", "AB型"]
-p type
+word = 'aaa'
+puts word
+puts word.upcase! #破壊的メソッド
+puts word
