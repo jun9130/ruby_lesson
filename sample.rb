46243c3a9e4cb68884c8baa16312f60036e03360
jun9130 <jun.nakama1030@gmail.com>
2020-09-26 22:49:32 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,6 +1,15 @@
-n = 1
+ary = [1, 2, 3]
+p ary[0]
+ary[0] = 100
+p ary[0]
+ary << 999
+p ary[3]
 
-4.times {
-  n += 1
-  puts n
-}
+a = [[1,2,3],["a","b","c"],["あ","い","う"]]
+p a[1][0]
+p a[5] #=> nil
+
+num = [1, 2, 3]
+p num
+num.delete_at(1)
+p num
