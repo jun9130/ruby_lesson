d14ac3f32dede47bd63c42037b04122471053942
jun9130 <jun.nakama1030@gmail.com>
2020-09-29 13:13:25 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,12 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-new_numbers = numbers.map {|n| n * 10} #numbersの各要素を10倍にしてnew_numbersに代入
-puts new_numbers # =>10, 20, 30, 40, 50 
+num = gets.to_i
 
-numbers = [1, 2, 3, 4, 5]
-new_numbers = numbers.collect {|n| n * 10}
-puts new_numbers
+ary = []
 
-numbers = [1, 2, 3, 4, 5]
-new_numbers = [] 
-numbers.each {|n| new_numbers << n * 10}
-puts new_numbers
+num.times {
+  ary << gets.to_i
+}
+
+puts ary.sort
+
