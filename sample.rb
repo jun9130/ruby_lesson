eec10520ddb0a57e14825ecd60beab6b1b4bdae4
jun9130 <jun.nakama1030@gmail.com>
2020-10-27 20:35:06 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,6 +1,12 @@
-array = [2, 4, 3, 0, 2, 0, 5, 0]
-puts array.sum.fdiv(array.length).floor(1)
+# array = [2, 4, 3, 0, 2, 0, 5, 0]
+# puts array.sum.fdiv(array.length).floor(1)
 
 
-ary = [2, 4, 3, 0, 2, 0, 5, 0]
-p ary.sum / ary.length
+# ary = [2, 4, 3, 0, 2, 0, 5, 0]
+# p ary.sum / ary.length
+
+def test
+  p 'OK'
+end
+
+test
