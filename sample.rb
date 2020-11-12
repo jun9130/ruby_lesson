0705b805caf7158ae8b10410868b1b17ad7f3f9b
jun9130 <jun.nakama1030@gmail.com>
2020-11-12 23:23:39 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,9 +1,5 @@
-# array = [2, 4, 3, 0, 2, 0, 5, 0]
-# puts array.sum.fdiv(array.length).floor(1)
-
-
-# ary = [2, 4, 3, 0, 2, 0, 5, 0]
-# p ary.sum / ary.length
-
-ary = [5, 4, 5]
-p ary.sum / ary.length
+num = []
+10.upto(15) {
+  |n| num << n
+}
+p num
