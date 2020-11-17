3ddecc4adc42f45114416e35b8a91295ec6b7842
jun9130 <jun.nakama1030@gmail.com>
2020-11-17 14:17:33 +0900

    practice

--- sample.rb
+++ sample.rb
@@ -1,16 +1,3 @@
-class Product
-  @name = 'Product'
-
-  def self.name
-    @name
-  end
-
-  def initialize(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-end
+puts "数字を入れてください"
+input_line = gets.to_i
+puts "#{input_line}週間後は#{input_line * 7}日後です。"
