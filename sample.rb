cab438c5dda31089d7d09a4be49d9814d81453aa
jun9130 <jun.nakama1030@gmail.com>
2020-11-20 22:20:04 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,3 +1,4 @@
-puts "数字を入れてください"
-input_line = gets.to_i
-puts "#{input_line}週間後は#{input_line * 7}日後です。"
+fruits = ['apple', 'orange', 'melon']
+fruits.each_with_index {
+  |fruit, i| puts "#{i}: #{fruit}"
+}
