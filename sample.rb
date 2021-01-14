2b4d0f3c37bf3711ac9d64fe6913b97443a9fd50
jun9130 <jun.nakama1030@gmail.com>
2021-01-14 22:59:55 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,13 +1,4 @@
-users = []
-users << {first_name: 'taro', last_name: 'Sato', age: 20}
-users << {first_name: 'jiro', last_name: 'Tanaka', age: 34}
-
-def full_name(user)
-  "#{user[:first_name]} #{user[:last_name]}"
+[5, 10, 15].each_with_index do |n, index|
+  p [n, index]
 end
-
-users.each do |user|
-  puts "#{full_name(user)}, #{user[:age]}" 
-end
-
 
