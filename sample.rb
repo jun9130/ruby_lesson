297d06da8a3b4c9bafd18269bf38300ea04e0a01
jun9130 <jun.nakama1030@gmail.com>
2021-02-18 14:11:19 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,40 +1,12 @@
-users = [
- {
-  id: 1,
-  name: "sato",
-  logs:[
-    {time: 20210123, score: 50},
-    {time: 20210104, score: 100},
-    {time: 20200304, score: 87},
-  ]
-},
-{
-  id: 2,
-  name: "inoue",
-  logs:[
-    {time: 20210123, score: 20},
-    {time: 20210104, score: 10},
-    {time: 20200304, score: 97},
-  ]
-},
-{
-  id: 3,
-  name: "mishima",
-  logs:[
-    {time: 20210123, score: 20},
-    {time: 20210104, score: 1220},
-    {time: 20200304, score: 81},
-  ]
-}
-]
+input_line = gets.chomp 
+word_count = input_line.length
+surround =  word_count + 2
+ 
+first_word = input_line.insert(0, "+")
 
-user_score = []
+first_word_count  = word_count + 1
+surround_word = first_word.insert(first_word_count ,"+")
 
-
-users.map do |user|
-  user[:logs].map do |score|
-    user_score << score[:score]
-  end
-end
-p user_score.max
-
+puts "+" * surround
+puts surround_word 
+puts "+" * surround
