a59c375384d9dc06b0297001c2f8025837da2922
jun9130 <jun.nakama1030@gmail.com>
2021-02-20 15:26:58 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,39 +1,29 @@
-input_line = []
-input_line = gets.chomp.chars
+input_line = gets.split(" ")
 
-word = []
+balance = input_line[0].to_i #残高
+ride = input_line[1].to_i #乗車回数
+point = 0 
+output = []
 
-input_line.each do |i|
-  if i == "A"
-    word << "4"
-  elsif i == "E"
-    word << "3"
-  elsif i == "G"
-    word << "6"
-  elsif i == "I"
-    word << "1"
-  elsif i == "O"
-    word << "0"
-  elsif i == "S"
-    word << "5"
-  elsif i == "Z"
-    word << "2"
+ride.times {
+  fare = gets.to_i
+  
+  if fare > point
+    balance = balance - fare
+    point = fare * 0.1
   else
-    word << i
+    balance = balance
+    point = point - fare
   end
-end
-
-puts word.join
+}
 
 
 
+# - 回数分繰り返す
+# - 運賃入力
+# - 運賃の10%をポイントに
+# - ポイントが運賃を上回る場合にポイントで支払う
 
 
 
-# A	4
-# E	3
-# G	6
-# I	1
-# O	0
-# S	5
-# Z	2
+
