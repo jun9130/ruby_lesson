9249b014bd9ac27a4dc0ef072adee2f998bf4010
jun9130 <jun.nakama1030@gmail.com>
2021-02-20 16:11:14 +0900

    運賃計算

--- sample.rb
+++ sample.rb
@@ -1,19 +1,27 @@
+puts "所持金と乗車回数を入れてください 例(2000 5)"
 input_line = gets.split(" ")
 
 balance = input_line[0].to_i #残高
 ride = input_line[1].to_i #乗車回数
-point = 0 
-
+total_point = 0 
 
 ride.times {
+  puts "運賃を入力してください"
   fare = gets.to_i
-  
-  if fare > point
+  if fare > total_point && balance > fare 
     balance = balance - fare
     point = fare * 0.1
-  else
+    total_point = total_point + point.to_i
+    puts "#{fare}円使ったので残高とポイントは以下の通りです"
+    puts [balance, total_point].each.map { |n| n}.join(" ")
+  elsif total_point > fare && total_point > fare
     balance = balance
-    point = point - fare
+    total_point = total_point - fare
+    puts "#{total_point}ポイント使ったので残高とポイントは以下の通りです"
+    puts [balance, total_point].each.map { |n| n}.join(" ")
+  else
+    puts "残高が足りません"
+    break
   end
 }
 
