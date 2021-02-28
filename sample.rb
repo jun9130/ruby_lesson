4acd382ebd68bb35e9727544c6a39e1ff0491323
jun9130 <jun.nakama1030@gmail.com>
2021-02-28 15:17:55 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,2 +1,19 @@
-puts [1, 2, 3].each.map {|n| n * 10 } 
+puts "日数を入力してください"
+days = gets.to_i
+
+days.times {
+  # 13:00 1 30
+  time = gets.split(" ")
+  #1
+  add_hour = time[1].to_i
+  #30
+  add_minute = time[2].to_i
+  #13,00
+  hour, minutes = time[0].split(":")
+  
+  if hour.to_i + add_hour > 24
+     puts "0#{0 + add_hour - 2}"
+  end 
+}
+
 
