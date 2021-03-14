3732d8701e08a164204324856d208de09898535f
jun9130 <jun.nakama1030@gmail.com>
2021-03-14 15:48:07 +0900

    カレンダー

--- sample.rb
+++ sample.rb
@@ -1,7 +1,23 @@
-(1..10).each {|n| p n}
-("a".."z").each {|n| p n}
+require 'date'
 
-(11...20).each {|n| p n}
+year_month = Date.today.strftime('%Y %b')
+year = Date.today.year
+month = Date.today.month
+first_week = Date.new(year, month, 1).wday
+last_day = Date.new(year, month, -1).day
 
+week = "Su Mo Tu We Th Fr Sa"
 
-p ("a".."g").class
+puts year_month
+puts week
+print "   " * first_week
+
+wday = first_week
+(1..last_day).each do |day|
+  print day.to_s.rjust(2) + " "    
+  wday = wday + 1
+  print "\n" if wday % 7 == 0                     
+end
+if wday % 7 != 0
+  print "\n"
+end
