85bd121c9e8516f3077e1223aa1adef934d53732
jun9130 <jun.nakama1030@gmail.com>
2021-03-20 14:55:55 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,3 +1,19 @@
-require 'date'
+def test(input)
+  output = []
 
-puts Date.today.strftime('%j')
+  input.each do |i|
+    if i.match(/\A(a|e|i|o|u|A|E|I|O|U|)\z/)
+     input.delete(i)
+    else
+     output << i
+    end
+  end
+  puts output.join
+
+end
+
+
+input = []
+input = gets.chomp.chars
+test(input)
+
