9538d22c6f2c92178fafad03fa9be638c7ba6bb7
jun9130 <jun.nakama1030@gmail.com>
2021-03-21 10:15:43 +0900

    leson

--- sample.rb
+++ sample.rb
@@ -1,37 +1,8 @@
-class User
-  attr_accessor :name
-end
+input = gets.to_i 
 
-user1 = User.new
-user1.name = '田中'
-user2 = User.new
-user2.name = '竹之下'
-user3 = User.new
-user3.name = '森'
+num = []
+output = gets.to_i
 
-users = [user1, user2, user3]
-
-puts users
-
-names = []
-
-# users.each do |user|
-#   names << user.name
-# end
-# puts names
-
-names = users.map do |user|
-  user.name #配列を返す
-end
-
-puts names
-
-puts [1, 2, 3].map {|n| n * 3 } 
-
-
-
-
-
-
+puts num.abs
 
 
