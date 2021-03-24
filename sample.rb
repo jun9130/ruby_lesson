ae337d82b440212fe002396ff1b9135d3e049c5a
jun9130 <jun.nakama1030@gmail.com>
2021-03-24 16:37:07 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,17 +1,9 @@
-num = gets.to_i
+input = gets
+num = gets.chomp.split(" ").map{|n| n.to_i }
 
-input = []
 output = []
-
-input << gets.chomp.split(" ")
-p input.class
-
-array = input.map{|n| n.to_i }
-p array.class
-
-array.each do |a|
-  output << a if a % 3 == 0 
+num.each do |n|
+  output << n if n % 3 == 0 
 end
 
 puts output.length
-
