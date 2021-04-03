a725cc876e4a19b8e8001d5a9da6a1cad3568255
jun9130 <jun.nakama1030@gmail.com>
2021-04-03 22:23:31 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,4 +1,4 @@
-input_num = gets.to_i
+input_num = gets.to_i 
 
 data = []
 input_num.times {
@@ -6,3 +6,15 @@ input_num.times {
 }
 
 name = gets.chomp
+
+i = 0
+
+input_num.times {
+  if name == data[i][0]
+    puts data[i][1]
+    return
+  else
+    i += 1
+  end
+}
+
