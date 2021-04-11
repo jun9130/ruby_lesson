0d6082ebf3656c1646d80b450aa32c3a4b634a6c
jun9130 <jun.nakama1030@gmail.com>
2021-04-11 23:18:14 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,20 +1,9 @@
-input_num = gets.to_i 
+input = gets.to_i
+puts input * 1500
 
-data = []
-input_num.times {
-  data << gets.chomp.split(" ")
-}
 
-name = gets.chomp
 
-i = 0
 
-input_num.times {
-  if name == data[i][0]
-    puts data[i][1]
-    return
-  else
-    i += 1
-  end
-}
+
+
 
