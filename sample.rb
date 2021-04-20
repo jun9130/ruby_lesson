503a03dd801ef4a7b113203a4a9836825b035263
jun9130 <jun.nakama1030@gmail.com>
2021-04-20 22:16:46 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,7 +1,15 @@
-integer = gets.to_i
+count = gets.to_i
+integers = []
 
-if integer >= 5
-  puts "high"
-else
-  puts "low"
-end
+
+count.times {
+ int1, int2 = gets.chomp.split(" ")
+ if int1 == int2
+  integers << int1.to_i * int2.to_i
+ else
+  integers << int1.to_i + int2.to_i
+ end
+}
+
+puts integers.sum
+
