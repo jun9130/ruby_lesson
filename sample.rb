44d53c2519fa62bb4a9e14397672aed1ec24cb0f
jun9130 <jun.nakama1030@gmail.com>
2021-04-23 14:21:50 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,8 +1,13 @@
-num1, num2 = gets.split(" ")
-int1 = num1.to_i - 1
-int2 = num2.to_i - 1
+ary = [1, 2, 3, 4, 5, 2]
+p ary
+p ary.uniq
 
-ary_string = gets.chars
+ary2 = ["a", "b", "a"]
+p ary2
+p ary2.uniq
 
-puts ary_string[int1 .. int2 ].join
 
+
+ary3 = [["php"],["バージョン3"]]
+p ary3
+p ary3.transpose
