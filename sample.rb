fbd4b051cd6e0002faf4f50b97a128751319c9fb
jun9130 <jun.nakama1030@gmail.com>
2021-04-24 21:34:53 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,13 +1,14 @@
-ary = [1, 2, 3, 4, 5, 2]
-p ary
-p ary.uniq
+input = gets.chars
+puts input[0]
 
-ary2 = ["a", "b", "a"]
-p ary2
-p ary2.uniq
+input1 = gets
+puts input1.upcase
+
+int1, int2 = gets.split(" ")
+(int1..int2).each do |i|
+  puts i
+end
 
 
 
-ary3 = [["php"],["バージョン3"]]
-p ary3
-p ary3.transpose
+
