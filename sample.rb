d6ebc3af47157f36ba4536ee4fc6c35a654f0283
jun9130 <jun.nakama1030@gmail.com>
2021-04-25 21:19:52 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,14 +1,2 @@
-input = gets.chars
-puts input[0]
-
-input1 = gets
-puts input1.upcase
-
-int1, int2 = gets.split(" ")
-(int1..int2).each do |i|
-  puts i
-end
-
-
-
-
+input = gets.chomp
+puts input.length
