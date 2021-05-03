710b57e4e195a0d5f2f50faea4e46273bf52f4f0
jun9130 <jun.nakama1030@gmail.com>
2021-05-03 21:52:22 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -7,3 +7,11 @@ puts input2[0]
 input3 = gets.chars
 puts input3
 
+input4 = gets.chars
+puts input4[0..1].join(" ")
+
+num = gets.to_i
+input5 = gets.chars
+
+num1 = num
+puts input5[num - 1..num1].join(" ")
