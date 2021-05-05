7a194a14e381ca3a2f00191f2f75e4cc5d1d20f3
jun9130 <jun.nakama1030@gmail.com>
2021-05-05 16:03:27 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,11 +1,16 @@
-num = gets.to_i
-puts num + 1
+# num = gets.to_i
+# puts num + 1
+
+# count = gets.to_i
+# int = []
+# count.times {
+#   input = gets.split(" ")
+#   int << input[1].to_i
+# }
+# puts int
 
 count = gets.to_i
-int = []
-count.times {
-  input = gets.split(" ")
-  int << input[1].to_i
-}
-puts int
-
+array = []
+array = gets.split(" ")
+int_ary = array.map(&:to_i) 
+puts int_ary.sort{|a, b| a.to_i <=> b.to_i }
