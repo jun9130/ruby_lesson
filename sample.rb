18bb7bdd535a38411c4bfacfd3b99f5b56bc7266
jun9130 <jun.nakama1030@gmail.com>
2021-05-05 15:43:34 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,18 +1,12 @@
-input = gets.chomp
-puts input.length
-
-input2 = gets.chars
-puts input2[0]
-
-input3 = gets.chars
-puts input3
-
-input4 = gets.chars
-puts input4[0..1].join(" ")
-
 num = gets.to_i
-input5 = gets.chars
-
+input5 = gets.chomp.chars
 num1 = num - 1
-num2 = num - 1
-puts input5[num1..num2].join(" ")
+num2 = num
+
+if num == input5.length
+  puts input5[num1].insert(1, " ")
+else
+  puts input5[num1..num2].join(" ")
+end
+
+
