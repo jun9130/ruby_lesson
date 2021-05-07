3f439755a561c6b59791a6d89242f9a8b326c5d9
jun9130 <jun.nakama1030@gmail.com>
2021-05-07 14:10:03 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,25 +1,2 @@
-# num = gets.chomp.to_i
-# array = {}
-
-# num.times {
-#   line = gets.chomp.split(' ')
-#   array[line[1].to_i] = line[0]
-# }
-
-# array = array.sort
-
-# array.each { |ele| puts ele[1] }
-
-
-# int = gets.chomp.to_i
-# str = gets.chomp.chars
-
-
-
-int = gets.chomp.to_i
-str = gets.chomp
-
-puts "#{str[int - 1]} #{str[int]}" if str[int]
-
-# 後置if
-
+p '0123'[2...3]
+p '0123'[2..3]
