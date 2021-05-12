cd8f875afe4f1f39f7f1079dc56d764618f6acdd
jun9130 <jun.nakama1030@gmail.com>
2021-05-12 15:08:26 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,7 +1,19 @@
 # input1 = gets.chomp
 # puts input1
 
-input = gets.chomp
-input_last = input.length - 1
+# input = gets.chomp
+# input_last = input.length - 1
 
-puts (input[0]..input[input_last]).to_a
+# puts (input[0]..input[input_last]).to_a
+
+
+input = gets.chomp
+n = 0
+score = []
+
+input.length.times {
+  score << input[n]
+  n += 1
+}
+
+p score
