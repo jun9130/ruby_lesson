42689fa86dca8996554d5a18a21ab8bcba88d3fb
jun9130 <jun.nakama1030@gmail.com>
2021-05-14 14:29:07 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,26 +1,2 @@
-# input1 = gets.chomp
-# puts input1
-
-# input = gets.chomp
-# input_last = input.length - 1
-
-# puts (input[0]..input[input_last]).to_a
-
-
-input = gets.chomp
-n = 0
-score = []
-
-input.length.times {
-  score << input[n]
-  n += 1
-}
-
-if score.include?("0")
-  puts "OK"
-else
-  puts "NG"
-end
-
-p score
-
+array = [1, 21, 3, 2, 1, 11, 2, 2,  ,2]
+puts array.delete(2) 
