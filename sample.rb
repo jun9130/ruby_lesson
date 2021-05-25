94e13156c227b6ae39185495c58bf3d0ee2214d1
jun9130 <jun.nakama1030@gmail.com>
2021-05-25 13:37:10 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,10 +1,24 @@
-hash = {
-  Kyoko: "B",
-  Rio: "O",
-  Tsubame: "AB",
-  KurodaSensei: "A",
-  NekoSensei:	"A"
-}
+input, input2 = gets.chomp.split(" ")
+puts input
+puts input2
 
-puts hash
-hash.each { |user, blood| puts "#{user} #{blood}" }
+int1, int2 = gets.split(" ")
+puts int1.to_i + int2.to_i
+
+word = gets.chomp
+puts word.length
+
+input = gets.chomp.chars
+puts input[0]
+
+input = gets.chomp
+puts input.upcase
+
+first, last = gets.split(" ")
+first_int = first.to_i
+last_int = last.to_i
+
+(first_int..last_int).each do
+  puts first_int
+  first_int += 1
+end
