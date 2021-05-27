2314c5b2dac0ffdf8b1782d343854ff5bc6f7b42
jun9130 <jun.nakama1030@gmail.com>
2021-05-27 22:40:14 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,24 +1,10 @@
-input, input2 = gets.chomp.split(" ")
-puts input
-puts input2
+dimensions = [
+  [10, 20],
+  [30, 40]
+]
 
-int1, int2 = gets.split(" ")
-puts int1.to_i + int2.to_i
-
-word = gets.chomp
-puts word.length
-
-input = gets.chomp.chars
-puts input[0]
-
-input = gets.chomp
-puts input.upcase
-
-first, last = gets.split(" ")
-first_int = first.to_i
-last_int = last.to_i
-
-(first_int..last_int).each do
-  puts first_int
-  first_int += 1
+areas = []
+dimensions.each do |length, width|
+  areas << length * width
 end
+puts areas
