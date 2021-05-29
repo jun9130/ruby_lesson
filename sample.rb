60033f6847857b7990a839723fdb8a742a30ccd6
jun9130 <jun.nakama1030@gmail.com>
2021-05-29 23:36:53 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,27 +1,15 @@
-w_inning_number = gets.to_s.strip.chomp
 counts = gets.to_i
-
-second_w_inning_number = w_inning_number.slice(2..7).to_i
-third_inning_number = w_inning_number.slice(3..7).to_i
-
 strings = []
-
-counts.times do
-  string = gets.strip.chomp
-  strings << string
+counts.times do |i|
+  string = gets.to_s.strip.downcase.chomp
+  if strings.include?(string)
+    strings.delete(string)
+    strings << string
+  else
+    strings << string
+  end
 end
 
-
-strings.each do |st|
-  if  st.chomp.to_i == w_inning_number.to_i
-    puts "first"
-  elsif  st.chomp.to_i  == w_inning_number.to_i + 1 or st.chomp.to_i  == w_inning_number.to_i - 1
-    puts "adjacent"
-  elsif  st.chomp.to_s.slice(2..7).to_i == second_w_inning_number
-    puts "second"
-  elsif  st.chomp.to_s.slice(3..7).to_i == third_inning_number
-    puts "third"
-  else
-    puts "blank"
-  end
+(strings.length-1).downto(0) do |i|
+  puts strings[i]
 end
