e31bab05d347de78a00c368460a35865686990e6
jun9130 <jun.nakama1030@gmail.com>
2021-05-29 21:49:31 +0900

    lesson

--- sample.rb
+++ sample.rb
@@ -1,14 +1,27 @@
-dimensions = [
-  [10, 20],
-  [30, 40]
-]
+w_inning_number = gets.to_s.strip.chomp
+counts = gets.to_i
 
-areas = []
-dimensions.each do |length, width|
-  areas << length * width
+second_w_inning_number = w_inning_number.slice(2..7).to_i
+third_inning_number = w_inning_number.slice(3..7).to_i
+
+strings = []
+
+counts.times do
+  string = gets.strip.chomp
+  strings << string
 end
-puts areas
 
-sum = 0
-10.times { |n| sum += n}
-puts sum
+
+strings.each do |st|
+  if  st.chomp.to_i == w_inning_number.to_i
+    puts "first"
+  elsif  st.chomp.to_i  == w_inning_number.to_i + 1 or st.chomp.to_i  == w_inning_number.to_i - 1
+    puts "adjacent"
+  elsif  st.chomp.to_s.slice(2..7).to_i == second_w_inning_number
+    puts "second"
+  elsif  st.chomp.to_s.slice(3..7).to_i == third_inning_number
+    puts "third"
+  else
+    puts "blank"
+  end
+end
